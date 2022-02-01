ff559442b55d7ef56b37886e73784f16743020aa
Alson33 <garbuja12alson@gmail.com>
2022-02-01 18:47:48 +0545

    add normal popup component

---
+++ lib/components/NormalPop.tsx
@@ -0,0 +1,35 @@
+import React, { RefObject, useEffect, useRef } from 'react';
+import { closePop } from '../helpers/popup';
+import '../styles/normalpop.module.css';
+
+const NormalPop = (
+    { message, type, delay }: 
+    { message: string, type: 'error'|'warning', delay?: number }
+  ) => {
+
+  const normalpopRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const closePopTime = setTimeout(() => closePop(normalpopRef), delay||4000)
+
+    return () => clearTimeout(closePopTime)
+  // eslint-disable-next-line
+  }, [])
+
+  return (
+        <div 
+          ref={normalpopRef}
+          className='normal-error--pop'
+          style={{
+            backgroundColor: type==='error'?'#f8d7da':'#fff3cd',
+            color: type==='error'?'#842029':'#664d03',
+            borderColor: type==='error'?'#f5c2c7':'#ffecb5',
+          }}
+        >
+            <p>{message}</p>
+            <span className='times-icon' onClick={() => closePop(normalpopRef)}>&times;</span>
+        </div>
+    );
+};
+
+export default NormalPop;
